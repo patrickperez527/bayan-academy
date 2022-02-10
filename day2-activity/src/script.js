8cc326c9a8ba0ca6c2fa382749175b72cbd69e23
John Patrick C. Perez <patrickperez527@gmail.com>
2022-02-10 22:11:54 +0800

    added button

--- day2-activity/src/script.js
+++ day2-activity/src/script.js
@@ -1,13 +1,12 @@
-const temp = prompt('Write your temperature');
+// Temperature activity
 
-// 37.3 up high
-// 36.5-37.2 normal
-// 36.4 below low
-
-if(temp <= 36.4){
-    alert('Your temperature is low');
-} else if (temp <= 37.2) {
-    alert('Your temperature is normal');
-} else {
-    alert('Your temperature is high');
-}
+function getTemperature() {
+    const temp = prompt('Write your temperature');
+    if(temp <= 36.4){
+        alert('Your temperature is low');
+    } else if (temp <= 37.2) {
+        alert('Your temperature is normal');
+    } else {
+        alert('Your temperature is high');
+    }
+  }
