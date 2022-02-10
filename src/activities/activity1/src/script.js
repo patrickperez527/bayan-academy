40307f398f4308c80fa62c501b8dd3e4e9af4254
John Patrick C. Perez <patrickperez527@gmail.com>
2022-02-11 02:29:11 +0800

    initial commit

--- src/activities/activity1/src/script.js
+++ src/activities/activity1/src/script.js
@@ -6,13 +6,20 @@ const inputTemp = document.querySelector('#temp');
 form.addEventListener('submit', e => {
     e.preventDefault();
     if(inputTemp.value === '') {
+        // returns if the input field is blank
         alert('Please input your temperature');
     } else if (inputTemp.value <= 36.4) {
+        // 36.4 below = low temperature
         alert('Your temperature is low');
     } else if (inputTemp.value <= 37.2) {
+        // 36.5 - 37.2 = normal temperature
         alert('Your temperature is normal');
-    } else {
+    } else if (inputTemp.value >= 37.3){
+        // 37.3 up = high temperature
         alert('Your temperature is high');
+    } else {
+        // returns if you enter a string
+        alert('Invalid temperature');
     }
 })
 
