1ea742465c76f965b18d41abb5a9ecd55dcacf41
John Patrick C. Perez <patrickperez527@gmail.com>
2022-02-11 20:55:19 +0800

    activity 2

---
+++ src/activities/activity2/src/script.js
@@ -0,0 +1,15 @@
+//CODING CHALLENGE
+//Create a program that will exit if the user enters an incorrect password thrice
+//If the user enters two incorrect passwords, but was able to provide the correct password for the 3rd time, reset your counter variable into 0
+//3 incorrect enter of password, add alert("Your account has been blocked!");
+
+//database password
+const userPass = "Password123";
+let inputPass = prompt("Please enter your password:");
+let counter = 0;
+
+while (inputPass != userPass > 3) {
+  inputPass = prompt("Please enter your password:"); 
+}
+
+console.log("Thank you for providing the right password!");
