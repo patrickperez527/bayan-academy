0dd7f79650c0d242d3ee5bf3dcdadd577a593b02
John Patrick C. Perez <patrickperez527@gmail.com>
2022-02-11 11:59:40 +0800

    minor changes

--- src/activities/activity1/src/script.js
+++ src/activities/activity1/src/script.js
@@ -2,24 +2,33 @@
 
 const form = document.querySelector('#my-form');
 const inputTemp = document.querySelector('#temp');
+const tempMsg = document.querySelector('#msg');
 
 form.addEventListener('submit', e => {
     e.preventDefault();
     if(inputTemp.value === '') {
         // returns if the input field is blank
-        alert('Please input your temperature');
+        alert('Input field cannot be blank.');
     } else if (inputTemp.value <= 36.4) {
         // 36.4 below = low temperature
-        alert('Your temperature is low');
+        // alert('Your temperature is low');
+        tempMsg.innerText = 'Your temperature is low.';
+        inputTemp.value = '';
     } else if (inputTemp.value <= 37.2) {
         // 36.5 - 37.2 = normal temperature
-        alert('Your temperature is normal');
+        // alert('Your temperature is normal');
+        tempMsg.innerText = 'Your temperature is normal.';
+        inputTemp.value = '';
     } else if (inputTemp.value >= 37.3){
-        // 37.3 up = high temperature
-        alert('Your temperature is high');
+        // 37.3 and above = high temperature
+        // alert('Your temperature is high');
+        tempMsg.innerText = 'Your temperature is high.';
+        inputTemp.value = '';
     } else {
         // returns if you enter a string
-        alert('Invalid temperature');
-    }
+        // alert('Invalid temperature');
+        tempMsg.style.color = 'red';
+        tempMsg.innerText = 'Invalid input';
+    }   
 })
 
