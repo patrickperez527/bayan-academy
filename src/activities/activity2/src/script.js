f225ec00faa8bb6efdaa2a7d43cab14a6d59d475
John Patrick C. Perez <patrickperez527@gmail.com>
2022-02-11 16:51:31 +0800

    minor changes

---
+++ src/activities/activity2/src/script.js
@@ -0,0 +1,37 @@
+// Temperature activity
+
+const form = document.querySelector('#my-form');
+const inputTemp = document.querySelector('#temp');
+const tempMsg = document.querySelector('#msg');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    // this is for white spaces 
+    inputTemp.value = inputTemp.value.replace(/^\s+|\s+$/g, "");
+
+    if(inputTemp.value == '') {
+        // returns if the input field is blank
+        // alert('Input field cannot be blank.');
+        tempMsg.innerText = 'Temperature field cannot be blank.';
+    } else if (inputTemp.value <= 36.4) {
+        // 36.4 below = low temperature
+        // alert('Your temperature is low');
+        tempMsg.innerText = 'Your temperature is low.';
+        inputTemp.value = '';
+    } else if (inputTemp.value <= 37.2) {
+        // 36.5 - 37.2 = normal temperature
+        // alert('Your temperature is normal');
+        tempMsg.innerText = 'Your temperature is normal.';
+        inputTemp.value = '';
+    } else if (inputTemp.value >= 37.3){
+        // 37.3 and above = high temperature
+        // alert('Your temperature is high');
+        tempMsg.innerText = 'Your temperature is high.';
+        inputTemp.value = '';
+    } else {
+        // returns if you enter a string or special characters
+        // alert('Invalid temperature');
+        tempMsg.innerText = 'The temperature must be an integer or a float!';
+    }   
+})
+
