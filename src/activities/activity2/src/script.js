3d5c4e690b47518a5bf2e437eb79a2625bb562f3
John Patrick C. Perez <patrickperez527@gmail.com>
2022-02-12 08:56:41 +0800

    minor changes

--- src/activities/activity2/src/script.js
+++ src/activities/activity2/src/script.js
@@ -1,9 +1,10 @@
-//CODING CHALLENGE
-//Create a program that will exit if the user enters an incorrect password thrice
-//If the user enters two incorrect passwords, but was able to provide the correct password for the 3rd time, reset your counter variable into 0
-//3 incorrect enter of password, add alert("Your account has been blocked!");
+// CODING CHALLENGE
+// Create a program that will exit if the user enters an incorrect password thrice
+// If the user enters two incorrect passwords, but was able to provide the correct password for the 3rd time, reset your counter variable into 0
+// 3 incorrect enter of password, add alert("Your account has been blocked!");
 
-//database password
+// Database password
+
 function myFunction(){
     const userPass = 'Password123';
     let inputPass = prompt('Please enter your password:');
