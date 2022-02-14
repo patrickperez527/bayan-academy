e8a61289c5890a6ed9ea80329bf43b3e9f946d69
John Patrick C. Perez <patrickperez527@gmail.com>
2022-02-14 21:51:54 +0800

    added comments

--- src/activities/activity4/src/script.js
+++ src/activities/activity4/src/script.js
@@ -1,12 +1,15 @@
+
 function getTemperature(tempValue){
     if(tempValue >= 37.2){
+        // returns high if the temperature 37.2 above
         return 'High Temperature';
     } else if (tempValue >= 36.4) {
+        // returns normal if the temperature is between 37.1 and 36.4
         return 'Normal Temperature';
     } else {
+        // returns low if the temperature is less than or equal 36.3
         return 'Low Temperature';
     }
 }
 
-console.log(getTemperature());
-
+console.log(getTemperature(36.3));
