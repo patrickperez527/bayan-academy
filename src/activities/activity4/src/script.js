5e44b48310f15b9a33f6034649e9da1ebfca60fd
John Patrick C. Perez <patrickperez527@gmail.com>
2022-02-14 23:03:26 +0800

    area-perimeter done and some minor changes

--- src/activities/activity4/src/script.js
+++ src/activities/activity4/src/script.js
@@ -1,15 +1,14 @@
-
 function getTemperature(tempValue){
     if(tempValue >= 37.2){
         // returns high if the temperature 37.2 above
-        return 'High Temperature';
+        return 'High body temperature';
     } else if (tempValue >= 36.4) {
         // returns normal if the temperature is between 37.1 and 36.4
-        return 'Normal Temperature';
+        return 'Normal body temperature';
     } else {
         // returns low if the temperature is less than or equal 36.3
-        return 'Low Temperature';
+        return 'Low body temperature';
     }
 }
-
-console.log(getTemperature(36.3));
+// test it here
+getTemperature();
---
+++ src/activities/activity5/src/script.js
@@ -0,0 +1,13 @@
+const square = {
+    area(side) {
+      // returns value squared
+      return side * side;
+    },
+    perimeter(side) {
+      // value times 4
+      return side * 4;
+    },
+};
+// test
+// square.area();
+// square.perimeter();
