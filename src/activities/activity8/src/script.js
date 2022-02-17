f552c7305a7e33ff5373dbeefc18e2d96ba77be8
John Patrick C. Perez <patrickperez527@gmail.com>
2022-02-17 18:51:03 +0800

    end

--- src/activities/activity8/src/script.js
+++ src/activities/activity8/src/script.js
@@ -1,6 +1,6 @@
 function reverseString(str) {
     var newString = "";
-    for (var i = str.length - 1; i >= 0; i--) {
+    for (let i = str.length - 1; i >= 0; i--) {
         newString += str[i];
     }
     return newString;
