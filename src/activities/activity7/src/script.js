1caf6d9944bbe59963b6271c7ffaa89450763535
John Patrick C. Perez <patrickperez527@gmail.com>
2022-02-17 18:49:18 +0800

    02/17/22 done

---
+++ src/activities/activity7/src/script.js
@@ -0,0 +1,9 @@
+function convertToF(celsius) {
+    return celsius * 9/5 + 32
+  }
+  
+console.log(convertToF(-30)); // -22
+console.log(convertToF(-10)); // 14
+console.log(convertToF(0)); // 32
+console.log(convertToF(20)); // 16
+console.log(convertToF(30)); // 86
---
+++ src/activities/activity8/src/script.js
@@ -0,0 +1,11 @@
+function reverseString(str) {
+    var newString = "";
+    for (var i = str.length - 1; i >= 0; i--) {
+        newString += str[i];
+    }
+    return newString;
+}
+
+console.log(reverseString('hello')); // olleh
+console.log(reverseString('Howdy')); // ydwoH
+console.log(reverseString('Greetings from Earth')); // htraE morf sgniteerG
