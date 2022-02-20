13ab6083cc99cbd77a5d2c8b60aac2ed12905613
John Patrick C. Perez <patrickperez527@gmail.com>
2022-02-20 20:25:16 +0800

    ewewew

--- src/activities/activity9/src/script.js
+++ src/activities/activity9/src/script.js
@@ -2,5 +2,5 @@ function allEvens(input) {
     return input.every(val => val % 2 === 0)
 }
 
-console.log(allEvens([2, 4, 6, 8])); // true
-console.log(allEvens([2, 3, 6, 8])); // false
+console.log(allEvens([2, 4, 6, 8, 10])); // true
+console.log(allEvens([2, 3, 6, 8, 5])); // false
