09ef7badb25afc995a69db7ddcd6c7c094d48941
John Patrick C. Perez <patrickperez527@gmail.com>
2022-03-02 20:15:06 +0800

    done

--- src/activities/activity18/src/script.js
+++ src/activities/activity18/src/script.js
@@ -11,9 +11,18 @@ const fakeRequestPromise = (url) => {
     })
 }
   
-async function makeRequests(url) {
-    await fakeRequestPromise(2000, 'page1');
-    console.log('here is your data: ', url)
+async function makeRequests() {
+    let data = await fakeRequestPromise('test.com');
+    console.log(data);
+
+    let data2 = await fakeRequestPromise('test.com');
+    console.log(data2);
+
+    let data3 = await fakeRequestPromise('test.com');
+    console.log(data3);
+
+    let data4 = await fakeRequestPromise('test.com');
+    console.log(data4);
 }
 
 makeRequests();
